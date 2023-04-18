73349ec4caa6df7efbe667d3d04b4a3618bbc123
Wesley Hughes <wesley_hughes@me.com>
2023-04-18 15:55:41 -0500

    updated all classes with SQL methods, expanded metal size and style within get all orders, created PUT for metals to allow price to be modified

--- models/order.py
+++ models/order.py
@@ -1,8 +1,11 @@
 class Order():
-    '''creates class'''
-    def __init__(self, id, metal_id, style_id, size_id, timestamp):
+    '''order class'''
+    def __init__(self, id, timestamp, style_id, metal_id, size_id):
         self.id = id
-        self.metal_id = metal_id
         self.style_id = style_id
+        self.metal_id = metal_id
         self.size_id = size_id
-        self.timestamp = ""
+        self.timestamp = timestamp
+        self.style = None
+        self.metal = None
+        self.size = None
